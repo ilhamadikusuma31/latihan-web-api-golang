60c28900a5827acd0c3a7ac7af7c35ef697e0957
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-07-21 14:29:10 +0700

    handler

--- main.go
+++ main.go
@@ -10,19 +10,21 @@ func main() {
 	fmt.Println("cek")
 	router := gin.Default()
 
-	router.GET("/", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"nama": "ilham adikuusuma",
-			"nim":  "202410103034",
-		})
-	})
-
-	router.GET("/player", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"id":    "20",
-			"uname": "hamadi",
-		})
-	})
-
+	router.GET("/", rootHandler)
+	router.GET("/player", playerHandler)
 	router.Run()
+}
+
+func rootHandler(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"nama": "ilham adikuusuma",
+		"nim":  "202410103034",
+	})
+}
+
+func playerHandler(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"id":    "20",
+		"uname": "hamadi",
+	})
 }
